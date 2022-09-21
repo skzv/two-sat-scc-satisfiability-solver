11c69a4a3cf70594a9672543e7f9c6902defa8a5
skz Kuznetsov <me@skz.dev>
2022-09-21 10:15:48 -0700

    implement TwoSatSatisfiabilitySolver and add data

--- src/dev/skz/Main.java
+++ src/dev/skz/Main.java
@@ -6,77 +6,50 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-         Graph graph = new Graph(9);
-         graph.addEdge(7, 1);
-         graph.addEdge(4, 7);
-         graph.addEdge(1, 4);
-         graph.addEdge(9, 7);
-         graph.addEdge(6, 9);
-         graph.addEdge(3, 6);
-         graph.addEdge(9, 3);
-         graph.addEdge(8, 6);
-         graph.addEdge(2, 8);
-         graph.addEdge(5, 2);
-         graph.addEdge(8, 5);
+        solve("test-data.txt");
+        solve("2sat1.txt");
+        solve("2sat2.txt");
+        solve("2sat3.txt");
+        solve("2sat4.txt");
+        solve("2sat5.txt");
+        solve("2sat6.txt");
+    }
 
-//        Graph graph = readGraphFromFile();
-        StronglyConnectedComponentComputer sccc = new StronglyConnectedComponentComputer(graph);
-
-        HashMap<Integer, HashSet<Integer>> components = sccc.getStronglyConnectedComponentsAsHashSets();
-        System.out.println("components: " + components);
-
-        // List<Integer> sizes = sccc.computeSizeOfStronglyConnectedComponents();
-        // List<Integer> top5sizes = sizes.subList(0, 5);
-        // System.out.println("top 5 sizes: " + top5sizes);
+    private static void solve(String filename) {
+        TwoSatSatisfiabilitySolver twoSatSatisfiabilitySolver = readTwoSatConstraintsFromFile(filename);
+        System.out.printf("Constraints for %s are satisfiable: %s%n", filename, twoSatSatisfiabilitySolver.isSatisfiable());
     }
 
     private static int[] getIntTokens(String line) {
         return Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
     }
 
-    private static int getNumberOfNodes(File file) {
-        HashSet<Integer> uniqueNodes = new HashSet<>();
+    private static TwoSatSatisfiabilitySolver readTwoSatConstraintsFromFile(String filename) {
+        File file = Paths.get("data/" + filename).toFile();
+
+        TwoSatSatisfiabilitySolver solver = null;
+
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
+
+            int size = Integer.parseInt(br.readLine());
+            solver = new TwoSatSatisfiabilitySolver(size);
+
             while ((line = br.readLine()) != null) {
                 int[] tokens = getIntTokens(line);
-                int u = tokens[0];
-                int v = tokens[1];
-                uniqueNodes.add(u);
-                uniqueNodes.add(v);
+                int a = tokens[0];
+                int b = tokens[1];
+                // System.out.printf("%d, %d %n", a, b);
+                solver.addClause(a, b);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return uniqueNodes.size();
-    }
-
-    private static Graph readGraphFromFile() {
-        File file = Paths.get("data/SCC.txt").toFile();
-        int numNodes = getNumberOfNodes(file);
-
-        Graph graph = new Graph(numNodes);
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                int[] tokens = getIntTokens(line);
-                int u = tokens[0];
-                int v = tokens[1];
-                graph.addEdge(u, v);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return graph;
+        return solver;
     }
 }
--- src/dev/skz/TwoSatSatisfiabilitySolver.java
+++ src/dev/skz/TwoSatSatisfiabilitySolver.java
@@ -1,48 +1,104 @@
 package dev.skz;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import static java.lang.Math.abs;
+
 public class TwoSatSatisfiabilitySolver {
     private final Graph graph;
+    private final int numVariables;
 
     public TwoSatSatisfiabilitySolver(int numVariables) {
+        this.numVariables = numVariables;
         graph = new Graph(2 * numVariables);
     }
 
     public void addClause(int a, int b) {
+        int A = abs(a);
+        int B = abs(b);
         if (a > 0) {
             if (b > 0) {
-                // a v b
-                graph.addEdge(-a, b);
-                graph.addEdge(-b, a);
+                // Case: a v b
+                // Implications:
+                // -a => b
+                // -b => a
+                graph.addEdge(mapVariableToGraphKey(-A), mapVariableToGraphKey(B));
+                graph.addEdge(mapVariableToGraphKey(-B), mapVariableToGraphKey(A));
             } else {
-                // a v -b
-                graph.addEdge(a, b);
-                graph.addEdge(-a, -b);
+                // Case: a v -b
+                // Implications:
+                // b => a
+                // -a => -b
+                graph.addEdge(mapVariableToGraphKey(B), mapVariableToGraphKey(A));
+                graph.addEdge(mapVariableToGraphKey(-A), mapVariableToGraphKey(-B));
             }
         } else {
             if ( b > 0) {
-                // -a v b
-                graph.addEdge(-a, -b);
-                graph.addEdge(b, a);
+                // Case: -a v b
+                // Implications:
+                // -a => -b
+                // b => a
+                graph.addEdge(mapVariableToGraphKey(A), mapVariableToGraphKey(B));
+                graph.addEdge(mapVariableToGraphKey(-B), mapVariableToGraphKey(-A));
             } else {
-                // -a v -b
-                graph.addEdge(a, -b);
-                graph.addEdge(b, -a);
+                // Case: -a v -b
+                // a => -b
+                // b => -a
+                graph.addEdge(mapVariableToGraphKey(A), mapVariableToGraphKey(-B));
+                graph.addEdge(mapVariableToGraphKey(B), mapVariableToGraphKey(-A));
             }
+        }
+    }
+
+    private int mapGraphKeyToVariable(int key) {
+        int a = key - numVariables;
+        if (a <= 0) {
+            a -= 1;
+        }
+        return a;
+    }
+
+    private int mapVariableToGraphKey(int a) {
+        // numVariables -> 2 * numVariables
+        // -numVariables -> 1
+        // -1 -> numVariables
+
+        // e.g. numVariables = 3
+        // -3 -2 -1  1  2  3
+        //  1  2  3  4  5  6
+        int key;
+        if (a < 0) {
+            key = a + numVariables + 1;
+        } else {
+            key = a + numVariables;
+        }
+        return key;
+    }
+
+    private void printComponents(Collection<HashSet<Integer>> components) {
+        for (HashSet<Integer> component : components) {
+            System.out.print("[ ");
+            for (int vertex : component) {
+                int variable = mapGraphKeyToVariable(vertex);
+                System.out.print(variable + ",");
+            }
+            System.out.print(" ]");
         }
     }
 
     public boolean isSatisfiable() {
         StronglyConnectedComponentComputer sccc = new StronglyConnectedComponentComputer(graph);
         HashMap<Integer, HashSet<Integer>> components = sccc.getStronglyConnectedComponentsAsHashSets();
-        System.out.println("components: " + components);
+        //printComponents(components.values());
 
         // The constraints are not satisfiable if the SCC contains contradicting implications.
         for (HashSet<Integer> component : components.values()) {
             for (int vertex : component) {
-                if (component.contains(-vertex)) {
+                int variable = mapGraphKeyToVariable(vertex);
+                int key = mapVariableToGraphKey(-variable);
+                if (component.contains(key)) {
                     return false;
                 }
             }
